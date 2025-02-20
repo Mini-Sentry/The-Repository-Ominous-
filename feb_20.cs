eb8368541c52361dba6176d9f6ddfd82682ce7ef
i AM ironman <GavinC061@stemcollegiate.ca>
2025-02-20 10:34:01 -0700

    Create feb_20.cs

---
+++ feb_20.cs
@@ -0,0 +1,11 @@
+//Today I learned how to declare variables, discovering the difference between int, float and string.
+
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
