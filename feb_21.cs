76cd682aa2396356c4c2f587059c4f041f728d63
i AM ironman <GavinC061@stemcollegiate.ca>
2025-02-21 08:58:07 -0700

    Create feb_21.cs

---
+++ feb_21.cs
@@ -0,0 +1,5 @@
+string firstName = "Gaybriel";
+int number = 675483;
+var temp = 109.9;
+
+Console.Write($"Hello, {firstName}! There are {number} messages in your inbox. The temperature is {temp} degrees celsius.");
