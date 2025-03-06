b5c588c0e35ae5190988fadabf1b866c679e3b8c
i AM ironman <GavinC061@stemcollegiate.ca>
2025-03-06 10:21:09 -0700

    Create Mar_6.cs

---
+++ Mar_6.cs
@@ -0,0 +1,10 @@
+Module 3, unit 1 & 2:
+What did you learn?
+
+I learned the difference between values and string literal equivalents.
+
+Console.WriteLine("123"); // String, doesn't hold any value, only text.
+Console.WriteLine(123); // A value that holds numerical data.
+
+Console.WriteLine("true"); // String
+Console.WriteLine(true); // Bool value
