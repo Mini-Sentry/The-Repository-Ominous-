3930fa994c10731c7850af311f839b2104ffd438
i AM ironman <GavinC061@stemcollegiate.ca>
2025-03-14 08:41:23 -0600

    Create Mar_14.cs

---
+++ Mar_14.cs
@@ -0,0 +1,8 @@
+// Module 4 unit 3 & 4
+// Learned how to add string and variables in one line of code.
+
+int version = 11;
+string updateText = "Update to Windows";
+Console.WriteLine($"{updateText} {version}!");
+
+// Result: Update to Windows 11!
