d27c008b247333bf0ee47ec53b67831f86ead4fa
i AM ironman <GavinC061@stemcollegiate.ca>
2025-04-08 12:17:26 -0600

    Create Apr_8.cs

---
+++ Apr_8.cs
@@ -0,0 +1,5 @@
+// Microsoft Learn Mod 4 Unit 5-8
+// Tutorial unit 5-8
+
+// In Mod 4 I learned how to use string interpolation and change between literal string and text by using special characters. 
+// I used simple string concatenation with the + symbol, and upgraded to string interpolation for combining values into a string template.
